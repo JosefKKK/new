72c0bcb5cf277f4661df438c2702452c6286d359
USER-I <ialyakin@ozon.ru>
2022-07-23 15:23:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide')
+
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+
+        clearClass()
+        slide.classList.add('active')
+            
+    })
+}
+
+function clearClass() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
